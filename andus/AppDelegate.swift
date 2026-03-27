656f58e0a19d51634c1e9be357c07b6343b9847c
Adriano Oliviero <adrianoliviero23@gmail.com>
2026-03-27 11:55:32 +0100

    Initial Commit

---
+++ andus/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  andus
+//
+//  Created by Adriano Oliviero on 27/03/26.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
